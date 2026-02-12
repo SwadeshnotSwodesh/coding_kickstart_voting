801030c145628f2a497f82032ec2ff148ee06631
SwadeshnotSwodesh <swodesthapa@gmail.com>
2026-02-13 00:34:33 +0545

    set timezone to Nepal

--- mysite/settings.py
+++ mysite/settings.py
@@ -156,11 +156,17 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
 
-USE_I18N = True
+TIME_ZONE = "Asia/Kathmandu"
 
 USE_TZ = True
+
+
+USE_I18N = True #Internationalization for translating text or supporting multiple languages
+USE_L10N = True #Localization for formatting dates, numbers, etc. based on locale
+
+
 
 
 # Static files (CSS, JavaScript, Images)
